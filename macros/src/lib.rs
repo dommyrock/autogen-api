566a58c9a694d4f9f20c990751f4a3666e2b3dc3
dommyrock <dominik.polzer@hotmail.com>
2024-02-26 14:46:24 +0100

    proc_macro attributes

---
+++ macros/src/lib.rs
@@ -0,0 +1,12 @@
+use proc_macro::TokenStream;
+
+//`proc-macro` crate types currently cannot export any items other than functions tagged with `#[proc_macro]`, `#[proc_macro_derive]`, or `#[proc_macro_attribute]`
+mod controllers;
+
+//macro registry example
+//https://github.com/cloudflare/foundations/tree/main/foundations-macros/src
+
+#[proc_macro_attribute]
+pub fn generate_controller(args: TokenStream, item: TokenStream) -> TokenStream {
+    controllers::expand(args)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,83 +1,32 @@
-use sqlx::{Row, SqlitePool};
+use axum::Router;
 
-//SQL AST
-use sqlparser::ast::{ObjectType, Statement};
-use sqlparser::dialect::GenericDialect;
-use sqlparser::parser::Parser;
+//local
+mod models;
 
 #[tokio::main]
 async fn main() -> Result<(), sqlx::Error> {
-    let pool = SqlitePool::connect("sqlite://db.db").await?;
-    let rows = sqlx::query(
-        "SELECT * FROM sqlite_master WHERE type='table' AND tbl_name NOT LIKE 'sqlite_%'",
-    )
-    .fetch_all(&pool)
-    .await?;
+    // let controllers: Vec<Box<dyn Controller>> = vec![
+    //     Box::new(MyModel1Controller),
+    //     Box::new(MyModel2Controller),
+    //     // Add more controllers here...
+    // ];
 
-    for row in rows {
-        println!("---------------------------------------");
-        let tbl_name: String = row.get("tbl_name");
-        let sql: String = row.get("sql");
+    // let app = register_controllers(Router::new(), controllers);
 
-        let dialect = GenericDialect {};
-        let ast: Vec<Statement> = Parser::parse_sql(&dialect, &sql).unwrap();
-        // println!("AST\n{:?}",ast);
+    //Router
+    //https://docs.rs/axum/latest/axum/struct.Router.html
 
-        if let Statement::CreateTable { name, columns, .. } = &ast[0] {
-            println!("Table Name: {}", name);
-            for column in columns {
-                println!(
-                    "Column Name: {}, Column Type: {}",
-                    column.name, column.data_type
-                );
-            }
-        }
+    async fn handler() {}
 
-        println!("\nTable Name: {},\nSQL: {}", tbl_name, sql);
-    }
+    let app = Router::new().fallback(handler);
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+
+    // let x = models::Location {id :12,state: "Arcansas".to_string()};
 
     Ok(())
 }
 
-
-//TODO use proc_macro to generate api endpoints using something like bellow + Axum
-
-// // In your proc_macro crate
-// extern crate proc_macro;
-// extern crate syn;
-
-// use proc_macro::TokenStream;
-// use quote::quote;
-// use syn::parse::{Parse, ParseStream, Result};
-// use syn::{parse_macro_input, Ident, LitStr, Token};
-
-// struct Table {
-//     name: LitStr,
-// }
-
-// impl Parse for Table {
-//     fn parse(input: ParseStream) -> Result<Self> {
-//         let name: LitStr = input.parse()?;
-//         Ok(Table { name })
-//     }
-// }
-
-// #[proc_macro]
-// pub fn create_struct(item: TokenStream) -> TokenStream {
-//     let Table { name } = parse_macro_input!(item as Table);
-
-//     let struct_name = Ident::new(&name.value(), name.span());
-//     let output: TokenStream = quote! {
-//         pub struct #struct_name {
-//             id: i32,
-//             name: String,
-//             surname: String,
-//             email: String,
-//         }
-//     }
-//     .into();
-//     output
-// }
-
-// // In your main crate
-// create_struct!("Candidate");
+struct MyModel1Controller {}
+struct MyModel2Controller {}
