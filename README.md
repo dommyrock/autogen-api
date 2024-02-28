a64a637112202805268a62bb6b568beea411266e
dommyrock <dominik.polzer@hotmail.com>
2024-02-28 20:09:10 +0100

    updated readme md

--- README.md
+++ README.md
@@ -65,3 +65,59 @@ Expands to
         ...
     };
 ```
+
+### Notes 
+
+>Unfortunately, procedural macros in Rust don’t have the ability to see the entire crate at once. 
+They are **invoked on a per-item basis**, meaning they only see the specific item they are applied to. 
+This is a limitation of the current design of procedural macros in Rust.
+
+There are workarounds to this limitation. 
+Another approach is to use a single macro invocation that takes the names of all the structs as arguments. This way, you can generate the code for all the structs at once.
+This approach I used here.
+
+Other approach is to use a separate build script (a build.rs file in your crate root) that scans your crate for items annotated with a specific attribute and generates code based on those items. This approach gives you more control and flexibility, but it also requires more setup and complexity.
+
+something like this 
+```rust
+use std::env;
+use std::fs::File;
+use std::io::{BufReader, Read};
+use std::path::Path;
+
+fn main() {
+    // Get the directory of the crate
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let crate_dir = Path::new(&out_dir).parent().unwrap().parent().unwrap().parent().unwrap();
+
+    // Read the contents of the crate
+    let mut file = BufReader::new(File::open(crate_dir.join("src/lib.rs")).unwrap());
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+
+    // Parse the contents of the crate
+    let syntax = syn::parse_file(&contents).unwrap();
+
+    // Iterate over the items in the crate
+    for item in syntax.items {
+        match item {
+            // If the item is a struct
+            syn::Item::Struct(item_struct) => {
+                // If the struct is annotated with the `#[generate_controller]` attribute
+                if item_struct.attrs.iter().any(|attr| attr.path.is_ident("generate_controller")) {
+                    // Generate the controller code for the struct
+                    let controller_code = generate_controller_code(&item_struct.ident);
+                    println!("{}", controller_code);
+                }
+            }
+            _ => {}
+        }
+    }
+}
+
+fn generate_controller_code(name: &syn::Ident) -> String {
+    // Generate the controller code for a struct with the given name
+    format!("impl Resource for {} {{}}", name)
+}
+
+```
--- autogen_macros/src/controllers.rs
+++ autogen_macros/src/controllers.rs
@@ -1,29 +1,12 @@
-/*
-The reason we need to use Box<dyn Controller> instead of just dyn Controller is because trait objects are dynamically sized types (DSTs), and Rust requires values to have a known size at compile time. By boxing the trait object, we’re storing it on the heap, which allows us to work with it as if it were a regular, statically sized type.
+use axum::http::{StatusCode, Uri};
+use axum::response::IntoResponse;
+use axum::routing::get;
+use axum::{Json, Router};
 
-In the context of the register_controllers function, we’re using Box<dyn Controller> so that we can store controllers of different types in the same vector. Each controller might be a different type, but they all implement the Controller trait, so we can treat them uniformly by boxing them and storing them as trait objects. This allows us to iterate over the vector and call methods on each controller, regardless of its concrete type. This is a form of polymorphism, a common pattern in object-oriented programming.
-*/
-
-use axum::Router;
 use proc_macro::TokenStream;
 use quote::quote;
 
-// fn register_controllers(router: Router<BoxRoute>, controllers: Vec<Box<dyn Controller>>) -> Router<BoxRoute> {
-//    let mut router = router;
-//    for controller in controllers {
-//        router = router.route(controller.path(), get(controller.get).post(controller.post));
-//    }
-//    router
-// }
-
-//Controllers should look somehting like this, meaning i should remove them from app/src/main.rs 
-/*
-pub trait Controller: DeserializeOwned + Send + 'static {}
-
-async fn get_resource<R: Controller>(Json(_payload): Json<R>) -> impl IntoResponse {
-    (StatusCode::OK, format!("Hi"))
-}
-*/
+//Turns out this is not so efficient , so for now im going in favour of declarative macro_rules
 
 pub(crate) fn expand(input: TokenStream) -> TokenStream {
     let ast: syn::DeriveInput = syn::parse(input).unwrap();
@@ -45,47 +28,52 @@ pub(crate) fn expand(input: TokenStream) -> TokenStream {
 
     gen.into()
 }
+trait Resource: Clone + Send + Sync + 'static {}
 
 pub fn generate_controller(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let input = syn::parse_macro_input!(item as syn::ItemStruct); //should be one of my struct types Location,Job,Candidate 
     let name = &input.ident;
     let output = quote! {
         #input //Original struct definition
-
-        impl Controller for #name { //impl Controller trait for each struct/model
-            fn path() -> &'static str {
-                stringify!(#name)
-            }
-
-            fn get(&self, _req: GetRequest) -> Result<GetResponse, String> {
-                Ok(GetResponse {
-                    message: format!("It works for {}.", stringify!(#name)),
-                })
-            }
-        }
+        //integrate "impl_resource_create_router" macro here 
+        
     };
     output.into()
 }
 
 
-// macro_rules! impl_resource_create_router {
-//     ($($model:ty),*) => {{
-//         $(
-//             impl Resource for $model {}
-//         )*
+macro_rules! impl_resource_create_router {
+    ($($model:ty),*) => {{
+        $(
+            impl Resource for $model {}
+        )*
 
-//         let mut router = Router::new();
-//         $(
-//             router = register::<$model>(router);
-//         )*
-//         router
-//     }};
-// }
+        let mut router = Router::new();
+        $(
+            router = register::<$model>(router);
+        )*
+        router
+    }};
+}
 
-// fn register<T: Resource>(router: Router) -> Router {
-//     let type_name = std::any::type_name::<T>();
-//     let path = format!("/{}", type_name.split("::").last().unwrap().to_lowercase());
-//     router
-//         .route(&path, get(get_resource::<T>).post(post::<T>))
-//         .fallback(fb::<T>)
-// }
+fn register<T: Resource>(router: Router) -> Router {
+    let type_name = std::any::type_name::<T>();
+    let path = format!("/{}", type_name.split("::").last().unwrap().to_lowercase());
+    router
+        .route(&path, get(get_resource::<T>).post(post::<T>))
+        .fallback(fb::<T>)
+}
+async fn get_resource<R: Resource>() -> impl IntoResponse {
+    let type_namespace_name = std::any::type_name::<R>();
+    let msg = format!("Hello, World! from --> {type_namespace_name}");
+    (StatusCode::OK, Json(msg))
+}
+async fn post<R: Resource>() -> impl IntoResponse {
+    (StatusCode::OK, "Entity updated")
+}
+async fn fb<R: Resource>(uri: Uri) -> impl IntoResponse {
+    (
+        StatusCode::NOT_FOUND,
+        format!("Route '{uri}' doesn't exist."),
+    )
+}
